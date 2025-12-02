4df0bdeb08db1c23cb77f1f8a4d5cdfff5256fdf
Nowak0 <kuubanowak@gmail.com>
2025-12-03 00:27:16 +0100

    partially done angular frontend

---
+++ lab/nbaProject/angular-frontend/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { FranchiseListComponent } from './components/franchise-list/franchise-list.component';
+import { FranchiseAddComponent } from './components/franchise-add/franchise-add.component';
+import { FranchiseEditComponent } from './components/franchise-edit/franchise-edit.component';
+import { FranchiseDetailsComponent } from './components/franchise-details/franchise-details.component';
+import { PlayerAddComponent } from './components/player-add/player-add.component';
+import { PlayerEditComponent } from './components/player-edit/player-edit.component';
+import { PlayerDetailsComponent } from './components/player-details/player-details.component';
+
+export const routes: Routes = [
+    {path: '', redirectTo: '/franchises', pathMatch: 'full'},
+    {path: 'franchises', component: FranchiseListComponent},
+    {path: 'franchises/add', component: FranchiseAddComponent},
+    {path: 'franchises/edit/:id', component: FranchiseEditComponent},
+    {path: 'franchises/details/:id', component: FranchiseDetailsComponent},
+    {path: 'franchises/:franchiseId/players/add', component: PlayerAddComponent},
+    {path: 'franchises/:franchiseId/players/edit/:id', component: PlayerEditComponent},
+    {path: 'franchises/:franchiseId/players/:playerId', component: PlayerDetailsComponent}
+];
---
+++ lab/nbaProject/angular-frontend/src/app/services/player.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PlayerListDTO, PlayerReadDTO, PlayerCreateUpdateDTO } from '../models/player.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PlayerService {
+    private apiUrl = "/api/players";
+
+    constructor(private http: HttpClient) {}
+
+    getAllPlayers(): Observable<PlayerListDTO[]> {
+        return this.http.get<PlayerListDTO[]>(this.apiUrl);
+    }
+
+    getPlayer(id: string): Observable<PlayerReadDTO> {
+        return this.http.get<PlayerReadDTO>(`${this.apiUrl}/${id}`);
+    }
+
+    getPlayersByFranchise(franchiseId: string): Observable<PlayerListDTO[]> {
+        return this.http.get<PlayerListDTO[]>(`${this.apiUrl}/franchise/${franchiseId}/players`);
+    }
+
+    createPlayer(franchiseId: string, player: PlayerCreateUpdateDTO): Observable<PlayerReadDTO> {
+        return this.http.post<PlayerReadDTO>(`${this.apiUrl}/${franchiseId}`, player);
+    }
+
+    updatePlayer(id: string, player: PlayerCreateUpdateDTO): Observable<PlayerReadDTO> {
+        return this.http.put<PlayerReadDTO>(`${this.apiUrl}/${id}`, player);
+    }
+
+    deletePlayer(id: string): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+
+    transferPlayer(playerId: string, newFranchiseId: string): Observable<PlayerReadDTO> {
+        return this.http.put<PlayerReadDTO>(`${this.apiUrl}/${playerId}/franchise/${newFranchiseId}`, {});
+    }
+}
